6695efd25ef17eec4d2c7976bce0c931fd6722b0
ayomidekappo12 <ayomidekappo12@gmail.com>
2025-01-14 22:22:21 +0100

    made changes to image import

--- my-portfolio/src/components/IconButton.tsx
+++ my-portfolio/src/components/IconButton.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import Image from "next/image";
+import Image from "next/legacy/image";
 
 interface IconButtonProps {
   value?: string;
